08e5692af6d74cff3af45de932501ea2e7ca1f37
briandev25 <briankips34@gmail.com>
2026-05-08 21:36:25 +0300

    Added bottomnavigationbar

--- lib/screens/home/homepage.dart
+++ lib/screens/home/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+import 'package:tumako/constants/app_colors.dart';
 import 'package:tumako/screens/home/widgets/profile_header.dart';
 import 'package:tumako/screens/home/widgets/user_actions.dart';
 
@@ -13,6 +15,21 @@ class Homepage extends StatelessWidget {
           child: Column(
             children: [ProfileHeader(), SizedBox(height: 20.0), UserActions()],
           ),
+        ),
+      ),
+      bottomNavigationBar: Container(
+        padding: EdgeInsets.all(10.0),
+        color: AppColors.secondaryColor,
+        child: GNav(
+          gap: 8.0,
+          tabBackgroundColor: Colors.grey.shade200,
+          tabs: [
+            GButton(icon: Icons.home, text: "Home"),
+            GButton(icon: Icons.local_shipping, text: "Shipping"),
+
+            GButton(icon: Icons.person, text: "Profile"),
+            GButton(icon: Icons.wallet, text: "wallet"),
+          ],
         ),
       ),
     );
