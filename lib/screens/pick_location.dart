a130b575fa64553fb1ba7667f264b3470bb3d7c2
briandev25 <briankips34@gmail.com>
2026-05-12 22:32:05 +0300

    Added Markers

--- lib/screens/pick_location.dart
+++ lib/screens/pick_location.dart
@@ -10,10 +10,37 @@ class PickLocation extends StatefulWidget {
 
 class _PickLocationState extends State<PickLocation> {
   late GoogleMapController _mapController;
+  Marker? _pickupLocationMarker;
+  Marker? _dropLocationMarker;
   static const _initialCameraPosition = CameraPosition(
     target: LatLng(37.773972, -122.431297),
     zoom: 10.0,
   );
+
+  void _addMarker(LatLng position) {
+    if (_pickupLocationMarker == null ||
+        (_pickupLocationMarker != null && _dropLocationMarker != null)) {
+      setState(() {
+        _pickupLocationMarker = Marker(
+          markerId: MarkerId("pickup"),
+          infoWindow: InfoWindow(title: "Pickup"),
+          icon: BitmapDescriptor.defaultMarkerWithHue(
+            BitmapDescriptor.hueGreen,
+          ),
+          position: position,
+        );
+      });
+    } else {
+      setState(() {
+        _dropLocationMarker = Marker(
+          markerId: MarkerId("drop"),
+          infoWindow: InfoWindow(title: "Drop"),
+          icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRed),
+          position: position,
+        );
+      });
+    }
+  }
 
   @override
   void dispose() {
@@ -32,6 +59,11 @@ class _PickLocationState extends State<PickLocation> {
         myLocationButtonEnabled: false,
         initialCameraPosition: _initialCameraPosition,
         onMapCreated: (controller) => _mapController = controller,
+        onLongPress: _addMarker,
+        markers: {
+          if (_pickupLocationMarker != null) _pickupLocationMarker!,
+          if (_dropLocationMarker != null) _dropLocationMarker!,
+        },
       ),
 
       floatingActionButton: FloatingActionButton(
